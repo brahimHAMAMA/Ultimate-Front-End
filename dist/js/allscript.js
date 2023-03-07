f51531874d3d3c840b53b4998b80c142bf0103d5
brahimHAMAMA <brahim.hamama@gmail.com>
2023-03-07 09:19:32 +0300

    Lesson 18

--- dist/js/allscript.js
+++ dist/js/allscript.js
@@ -11,5 +11,37 @@ $(function(){
         $(this).children('.toggle-submenu').toggleClass('fa-angle-down fa-angle-right');
         console.log('Good');
     });
+    // Toggle Full Screen
+    $('.toggle-fullscreen').on('click', function(){
+        $('.toggle-fullscreen').toggleClass("fullscreen");
+        ($(this).hasClass('fullscreen'))?openFullscreen(): closeFullscreen()
+    });
+
 });
+
+        /* Get the documentElement (<html>) to display the page in fullscreen */
+        var elem = document.documentElement;
+
+        /* View in fullscreen */
+        function openFullscreen() {
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.webkitRequestFullscreen) { /* Safari */
+                elem.webkitRequestFullscreen();
+            } else if (elem.msRequestFullscreen) { /* IE11 */
+                elem.msRequestFullscreen();
+            }
+        }
+        
+        /* Close fullscreen */
+        function closeFullscreen() {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) { /* Safari */
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { /* IE11 */
+                document.msExitFullscreen();
+            }
+        }
+
 
